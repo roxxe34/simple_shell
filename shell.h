13306e4864e4d40e4802d63ce6dd5f3413290376
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-07-18 00:52:17 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -2,10 +2,11 @@
 #define SHELL_H
 
 #include <sys/wait.h>
+
 #define MAX_TOKENS 100
 
 
-int tokenize(char *buffer, char *tokens[]);
+int tokenize(char *buffer, char *tokens[])
 void execute_command(char *tokens[]);
 int _strcmp(char *s1, char *s2);
 
