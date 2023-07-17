17ca49b06b96f367be79449d93a97cb5a920c5fa
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-07-17 15:27:08 +0100

    Update and rename _getline.c to _functions.c

---
+++ _functions.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void execute_command(char *tokens[])
+{
+    pid_t pid = fork();
+    if (pid == -1)
+    {
+        perror("fork");
+        return;
+    }
+    else if (pid == 0)
+    {
+        if (execvp(tokens[0], tokens) == -1)
+        {
+            fprintf(stderr, "%s: Command not found\n", tokens[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        int status;
+        wait(&status);
+    }
+}
+void tokenize(char *buffer, char *tokens[], int *count)
+{
+    char *delim = " \n";
+    *count = 0;
+
+    char *token = strtok(buffer, delim);
+    while (token != NULL && *count < MAX_TOKENS)
+    {
+        tokens[(*count)++] = token;
+        token = strtok(NULL, delim);
+    }
+}
