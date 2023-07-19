34392c419c1a5c74b0e6d58ea366e68f5f21ed29
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-07-20 00:52:43 +0100

    optimze

--- simple.c
+++ simple.c
@@ -1,34 +1,5 @@
 #include "shell.h"
 /**
- * my_strncmp - Compare the first 'n' characters of two strings.
- * @str1: Pointer to the first string.
- * @str2: Pointer to the second string.
- * @n: Number of characters to compare.
- *
- *	Return: 0 if the strings are equal up to 'n' characters.
- *	A negative value if the first string is lexicographically
- *	less than the second string.
- *	A positive value if the first string is lexicographically
- *	greater than the second string.
- */
-
-int my_strncmp(const char *str1, const char *str2, size_t n)
-{
-	size_t i;
-
-	for (i = 0; i < n; i++)
-	{
-		if (str1[i] != str2[i])
-		{
-			return (str1[i] - str2[i]);
-		} else if (str1[i] == '\0')
-		{
-			return (0);
-		}
-	}
-	return (0);
-}
-/**
  * _strlen - Calculate the length of a null-terminated string.
  * @str: Pointer to the string.
  *
@@ -63,9 +34,32 @@ char *_strcpy(char *dest, const char *src)
 	dest[a] = '\0';
 	return (dest);
 }
+/**
+ * handle_exit_command - Handle the "exit" command and exit the shell.
+ * @tokens: An array of tokens representing the command and arguments.
+ * @count: The number of tokens in the array.
+ */
+void handle_exit_command(char **tokens, int count)
+{
+	if (count == 2 && is_number(tokens[1]))
+	{
+		int status = _atoi(tokens[1]);
 
-
-int main(void)
+		freetok(tokens, count);
+		my_exit(status);
+	} else if (count == 1)
+	{
+		freetok(tokens, count);
+		my_exit(0);
+	} else
+	{
+		fprintf(stderr, "Invalid usage of 'exit'. Usage: exit status\n");
+	}
+}
+/**
+ * read_input_and_execute - Read user input and execute commands.
+ */
+void read_input_and_execute(void)
 {
 	char *line = NULL;
 	size_t len = 0;
@@ -94,28 +88,21 @@ int main(void)
 		{
 			if (_strcmp(tokens[0], "exit") == 0)
 			{
-				if (count == 2 && is_number(tokens[1]))
-				{
-					int status = _atoi(tokens[1]);
-
-					free(line);
-					freetok(tokens, count);
-					my_exit(status);
-				} else if (count == 1)
-				{
-					free(line);
-					freetok(tokens, count);
-					my_exit(0);
-				} else
-				{
-					fprintf(stderr, "Invalid usage of 'exit'. Usage: exit status\n");
-				}
+				handle_exit_command(tokens, count);
 			} else
 			{
 				execute_command(tokens);
-				freetok(tokens, count);
 			}
+			freetok(tokens, count);
 		}
 	}
+}
+/**
+ * main - Main function that starts the simple shell.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	read_input_and_execute();
 	return (0);
 }
