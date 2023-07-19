96cb39fde87e92f94941b5656df1db60a9dee038
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-07-20 00:27:18 +0100

    Add files via upload

--- utils.c
+++ utils.c
@@ -1,17 +1,29 @@
 #include "shell.h"
-
-int is_number(const char* str)
+/**
+ * is_number - Check if a string is a valid number.
+ * @str: The string to check
+ *
+ * Return: 1 if the string is a number, 0 otherwise.
+ */
+int is_number(const char *str)
 {
 	int i;
-	for (i = 0; str[i] != '\0'; i++) {
-		if (!isdigit(str[i])) {
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (!isdigit(str[i]))
 			return (0);
-		}
 	}
 	return (1);
 }
-
-
+/**
+ * _str_search - Search for the first occurrence of a character in a string.
+ * @s: The string to search
+ * @c: The character to find
+ *
+ * Return: A pointer to the first occurrence of the character in the string,
+ *         or NULL if the character is not found.
+ */
 char *_str_search(char *s, char c)
 {
 	int i = 0;
@@ -67,26 +79,38 @@ int _atoi(char *s)
  * @str: char
  * Return: 0
  */
-char* _strdup(const char* str)
+char *_strdup(const char *str)
 {
 	size_t len;
-	char* new_str;
-	if (str == NULL) {
+	char *new_str;
+
+	if (str == NULL)
+	{
 		return (NULL);
 	}
 	len = _strlen(str);
-	new_str = (char*)malloc((len + 1) * sizeof(char));
+	new_str = (char *)malloc((len + 1) * sizeof(char));
 
-	if (new_str != NULL) {
+	if (new_str != NULL)
+	{
 
 		_strcpy(new_str, str);
 	}
 
 	return (new_str);
 }
+/**
+ * _strcmp - Compare two strings.
+ * @s1: The first string to compare
+ * @s2: The second string to compare
+ *
+ * Return: An integer less than, equal to, or greater than zero if s1 is found,
+ *         respectively, to be less than, to match, or be greater than s2.
+ */
 int _strcmp(const char *s1, const char *s2)
 {
-	while (*s1 && *s2 && *s1 == *s2) {
+	while (*s1 && *s2 && *s1 == *s2)
+	{
 		s1++;
 		s2++;
 	}
